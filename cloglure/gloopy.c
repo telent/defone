7bdb1c51e05a20bdf50c6c15b539218375db97f7
Daniel Barlow <dan@telent.net>
2014-03-09 11:31:07 +0000

    draw() function is example not lib

--- cloglure/gloopy.c
+++ cloglure/gloopy.c
@@ -243,41 +243,6 @@ egl_destroy(void)
    egl_dpy = EGL_NO_DISPLAY;
 }
 
-/* stolen from tri.c */
-static void
-draw(int frame)
-{
-   static const GLfloat verts[3][2] = {
-      { -1, -1 },
-      {  1, -1 },
-      {  0,  1 }
-   };
-   static const GLfloat colors[3][4] = {
-      { 1, 0, 0, 1 },
-      { 0, 1, 0, 1 },
-      { 0, 0, 1, 1 }
-   };
-   GLfloat view_rotz = (GLfloat) frame;
-
-   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-   glPushMatrix();
-   glRotatef(view_rotz, 0, 0, 1);
-
-   glVertexPointer(2, GL_FLOAT, 0, verts);
-   glColorPointer(4, GL_FLOAT, 0, colors);
-
-   glEnableClientState(GL_VERTEX_ARRAY);
-   glEnableClientState(GL_COLOR_ARRAY);
-
-   /* draw triangle */
-   glDrawArrays(GL_TRIANGLES, 0, 3);
-
-   glDisableClientState(GL_VERTEX_ARRAY);
-   glDisableClientState(GL_COLOR_ARRAY);
-
-   glPopMatrix();
-}
 
 static void
 init(int width, int height)
