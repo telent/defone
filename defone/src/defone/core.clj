03f3089470b01fe6748812622f10e1c73d115744
Daniel Barlow <dan@telent.net>
2014-03-12 00:16:04 +0000

    start xlating cloglure example.c into clojure
    Finding exciting stuff I didn't know about calling convention. Lesson
    One: int arguments to jna calls need to be cast to int lest they be
    assumed long and allocated two registers when the callee expects only
    one

--- defone/src/defone/core.clj
+++ defone/src/defone/core.clj
@@ -1,6 +1,7 @@
 (ns defone.core
   (:require [clojure.core.async :as async
              :refer [chan >!! <!! >! <! go ]]
+            [net.n01se.clojure-jna :as jna]
             [clojure.java.io :as io])
   (:import [java.io FileReader FileInputStream]))
 
@@ -97,3 +98,103 @@
 
 #_
 (read-stuff)
+
+;;;;;;;;;;;;;; some opengl stuff
+
+(defn matrix-rotate-z [angle]
+  (let [c (Math/cos angle)
+        s (Math/sin angle)]
+    [[ c     s    0    0]
+     [(- s)  c    0    0]
+     [ 0     0    1    0]
+     [ 0     0    0    1]]))
+
+(defn matrix-scale [xs ys zs]
+  [[xs 0  0  0]
+   [ 0 ys 0  0]
+   [ 0 0  zs 0]
+   [ 0 0  0  1]])
+
+;; matrix multiplication in baby steps
+
+(defn matrix-transpose [a]
+  (apply map vector a))
+
+(defn matrix-multiply-cell [row-from-a col-from-b]
+  (apply + (map * row-from-a col-from-b)))
+
+(defn matrix-multiply-row [row-from-a b]
+  (map #(matrix-multiply-cell row-from-a %) (matrix-transpose b)))
+
+(defn matrix-multiply [a b]
+  (map #(matrix-multiply-row % b) a))
+
+(assert (= '((2 2)
+             (2 2))
+           (matrix-multiply
+            [[2 0]
+             [0 2]]
+            [[1 1]
+             [1 1]])))
+
+
+;;;
+
+(def triangle-verts
+  [[-1, -1],
+   [1,  -1],
+   [0,   1]])
+
+(def triangle-colors
+  [[1, 0, 0, 1],
+   [0, 1, 0, 1],
+   [0, 0, 1, 1]])
+
+(def GL_COMPILE_STATUS (int 0x8b81))
+
+(defn gl-make-shader [type text]
+  (let [typenum (get {:fragment 0x8B30 :vertex 0x8B31} type)
+        shader (int (jna/invoke Integer GLESv2/glCreateShader typenum))
+        string (clojure.string/join "\n" text)
+        compiled? (int-array [42])]
+    (jna/invoke Integer GLESv2/glShaderSource
+                shader
+                (int 1)
+                (into-array String [string])
+                (int 0))
+    (jna/invoke Integer GLESv2/glCompileShader shader)
+    (jna/invoke Integer GLESv2/glGetShaderiv shader GL_COMPILE_STATUS compiled?)
+    (println (seq compiled?))
+    (and (not (zero? (aget compiled? 0))) shader)))
+
+
+(defn create-shaders []
+  (let [frag (gl-make-shader
+              :fragment
+              ["precision mediump float;"
+               "varying vec4 v_color;"
+               "void main() {"
+               "gl_FragColor = v_color;"
+               "}"])
+        vert (gl-make-shader
+              :vertex
+              ["uniform mat4 modelviewProjection;\n"
+               "attribute vec4 pos;\n"
+               "attribute vec4 color;\n"
+               "varying vec4 v_color;\n"
+               "void main() {\n"
+               "   gl_Position = modelviewProjection * pos;\n"
+               "   v_color = color;\n"
+               "}\n"])]
+    [frag vert]))
+
+
+(defn gl-triangle [keep]
+  (let [fb0 (jna/invoke Integer cloglure/cloglure_start "/dev/graphics/fb0")]
+    (println fb0)
+    (jna/invoke Integer cloglure/cloglure_swap_buffers)
+    (or keep
+        (jna/invoke Integer cloglure/cloglure_stop fb0))))
+
+#_
+(gl-triangle)
