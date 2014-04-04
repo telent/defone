1eea9a612b6966f988298ec574c01406118bdacf
Daniel Barlow <dan@telent.net>
2014-04-04 14:55:27 +0000

    works intermittently :-(

--- defone/src/defone/ui.clj
+++ defone/src/defone/ui.clj
@@ -1,242 +1,217 @@
 (ns defone.ui
   (:require [defone.matrix :as m]
+            [defone.glj :as glj]
             [clojure.core.async :as async
              :refer [chan >!! <!! >! <! go ]]
             [net.n01se.clojure-jna :as jna])
   (:import [java.io File FileInputStream]))
 
 
-(jna/to-ns clogl cloglure [Integer cloglure_start,
-                           Integer cloglure_stop,
-                           Integer cloglure_swap_buffers,
-                           Integer cloglure_get_display])
-(jna/to-ns egl EGL [Integer eglGetError])
-(jna/to-ns gl GLESv2 [Integer glUniformMatrix4fv,
-                      Integer glUniform1i,
-                      Integer glUniform4fv,
-                      Integer glVertexAttribPointer,
-                      Integer glEnableVertexAttribArray,
-                      Integer glDrawArrays,
-                      Integer glDisableVertexAttribArray,
-                      Integer glActiveTexture,
-                      Integer glBindTexture,
-                      Integer glGetError,
-                      Integer glClear])
-
-(def GL_COMPILE_STATUS (int 0x8b81))
-(def GL_LINK_STATUS (int 0x8B82))
-(def GL_FLOAT (int 0x1406))
-(def GL_COLOR_BUFFER_BIT 0x00004000)
-(def GL_DEPTH_BUFFER_BIT 0x00000100)
-
-(def GL_POINTS                         (int 0x0000))
-(def GL_LINES                          (int 0x0001))
-(def GL_LINE_LOOP                      (int 0x0002))
-(def GL_LINE_STRIP                     (int 0x0003))
-(def GL_TRIANGLES                      (int 0x0004))
-(def GL_TRIANGLE_STRIP                 (int 0x0005))
-(def GL_TRIANGLE_FAN                   (int 0x0006))
-
-(def GL_TEXTURE_2D (int 0x0DE1))
-(def GL_TEXTURE_MIN_FILTER (int 0x2801))
-(def GL_NEAREST (int 0x2600))
-(def GL_RGBA (int 0x1908))
-(def GL_UNSIGNED_BYTE (int 0x1401))
-
-(def GL_TEXTURE0 (int 0x84c0))
-(def GL_TEXTURE1 (int 0x84c1))
-(def GL_TEXTURE2 (int 0x84c2))
-(def GL_TEXTURE3 (int 0x84c3))
-(def GL_TEXTURE4 (int 0x84c4))
-
-(defn gl-shader-type [name]
-  (int (get {:fragment 0x8B30 :vertex 0x8B31} name)))
-
-(defn gl-make-shader [type text]
-  (let [typenum (gl-shader-type type)
-        shader (int (jna/invoke Integer GLESv2/glCreateShader typenum))
-        string (clojure.string/join "\n" text)
-        compiled? (int-array [42])]
-    (jna/invoke Integer GLESv2/glShaderSource
-                shader
-                (int 1)
-                (into-array String [string])
-                (int 0))
-    (jna/invoke Integer GLESv2/glCompileShader shader)
-    (jna/invoke Integer GLESv2/glGetShaderiv shader GL_COMPILE_STATUS compiled?)
-    (if (zero? (aget compiled? 0))
-      (let [err (byte-array 1000)
-            len (int-array [0])]
-        (jna/invoke Integer GLESv2/glGetShaderInfoLog
-                    shader (int 1000) len err)
-        (println (clojure.string/trim (String. err)))))
-    (and (not (zero? (aget compiled? 0))) shader)))
-
-(defn gl-make-program [shaders]
-  (let [program (int (jna/invoke Integer GLESv2/glCreateProgram))
-        success? (int-array [0])]
-    (doall (map #(jna/invoke Integer GLESv2/glAttachShader program %) shaders))
-    (jna/invoke Integer GLESv2/glLinkProgram program)
-    (jna/invoke Integer GLESv2/glGetProgramiv program GL_LINK_STATUS success?)
-    (if (zero? (aget success? 0))
-      (let [err (byte-array 1000)
-            len (int-array [0])]
-        (jna/invoke Integer GLESv2/glGetProgramInfoLog
-                    program (int 1000) len err)
-        (println (clojure.string/trim (String. err)))
-        nil)
-      (do (jna/invoke Integer GLESv2/glUseProgram program) program))))
-
-(defn create-shaders []
-  (let [frag (gl-make-shader
-              :fragment
-              ["precision mediump float;"
-               "varying vec4 v_color;"
-               "void main() {"
-               "  gl_FragColor = v_color;"
-               "}"])
-        vert (gl-make-shader
-              :vertex
-              ["uniform mat4 modelviewProjection;"
-               "attribute vec4 pos;"
-               "uniform vec4 color;"
-               "varying vec4 v_color;"
-               "void main() {"
-               "   gl_Position = modelviewProjection * pos;"
-               "   v_color = color;"
-               "}"])]
-    (assert frag)
-    (assert vert)
-    (gl-make-program [frag vert])))
+(defn compile-glsl-program [{:keys [attributes uniforms varyings shaders]}]
+  (let [format-decls (fn [vtype mapp]
+                       (map (fn [[n t]] (print-str vtype (name t) (name n) ";"))
+                            mapp))
+        fragment-shader-src (concat ["precision mediump float;"]
+                                    (format-decls "uniform" uniforms)
+                                    (format-decls "varying" varyings)
+                                    (:fragment shaders))
+        fragment-shader (glj/make-shader :fragment fragment-shader-src)
+        vertex-shader-src (concat ["precision mediump float;"]
+                                  (format-decls "uniform" uniforms)
+                                  (format-decls "varying" varyings)
+                                  (format-decls "attribute" attributes)
+                                  (:vertex shaders))
+        vertex-shader (glj/make-shader :vertex vertex-shader-src)
+        program (glj/make-program [fragment-shader vertex-shader])]
+    {:index program
+     :uniforms (reduce (fn [m n] (assoc m
+                                   n (glj/uniform-index program (name n))))
+                       {}
+                       (keys uniforms))
+     :attributes (reduce (fn [m n] (assoc m
+                                     n (glj/attribute-index program (name n))))
+                         {}
+                         (keys attributes))
+     }))
 
 
-(defn flat-float-array [matrix]
-  (float-array (flatten matrix)))
-
-(defn gl-uniform-matrix [index matrix]
-  (gl/glUniformMatrix4fv
-   (int index) (int 1) (int 0) (flat-float-array matrix)))
-
-(defn gl-uniform4 [index vals]
-  (gl/glUniform4fv (int index) (int 1) (flat-float-array vals)))
-
-(defn gl-attribute-index [program name]
-  (let [i (jna/invoke Integer GLESv2/glGetAttribLocation program name)]
-    (and (>= i 0) (int i))))
-
-(defn gl-uniform-index [program name]
- (let [i (jna/invoke Integer GLESv2/glGetUniformLocation program name)]
-    (and (>= i 0) (int i))))
-
-;;;;;;
 
 (defmulti draw-scene (fn [context key & more] key))
 
-(defn draw-vertices [context draw-mode vertices]
-  ;; we could maybe be more efficient by calling gl-uniform-matrix
-  ;; lazily but let's try it the easy way first
-  (let [indices (:indices context)]
-    (gl-uniform-matrix (:mvp indices) (:transform context))
-    (gl-uniform4 (:color indices) (:color context))
+
+(defn draw-vertices [context draw-mode attributes]
+  (let [vars (:program context)]
+    (glj/uniform-matrix (:mvp (:uniforms vars)) (:transform context))
+
+    (if-let [col (:color (:uniforms vars))]
+      (glj/uniform4 col (:color context)))
 
     ;; XXX I suspect this only works by accident.  Last arg is
     ;; supposed to be "offset of the first component of the first
     ;; generic vertex attribute in the array in the data store of the
     ;; buffer currently bound to the GL_ARRAY_BUFFER target", but we have
-    ;; not bound any buffers, so ... probably only works because we have
+    ;; not bound any buffers, so ... perhaps only works because we have
     ;; software-only mesa and "gpu" memory is system memory
+    (doall
+     (map (fn [attribute]
+            (let [data (get attributes attribute)
+                  index (get (:attributes vars) attribute)
+                  size (count (first data))]
+              (println [:index index
+                        :data (seq (glj/flat-float-array data))
+                        :size size])
+              (gl/glVertexAttribPointer index
+                                        (int size)
+                                        glj/GL_FLOAT (int 0) (int 0)
+                                        (glj/flat-float-array data))
+              (gl/glEnableVertexAttribArray index)
+              (println (gl/glGetError))))
+          (keys attributes)))
 
-    (gl/glVertexAttribPointer (:pos indices)
-                              (int 3)
-                              GL_FLOAT (int 0) (int 0)
-                              (flat-float-array vertices))
-    (gl/glEnableVertexAttribArray (:pos indices))
-    (gl/glDrawArrays draw-mode (int 0) (int (count vertices)))
-    (gl/glDisableVertexAttribArray (:pos indices))))
+    (let [len (count (first (vals attributes)))]
+      (println [:len len])
+      (gl/glDrawArrays draw-mode (int 0) (int len)))
+
+    (doall
+     (map (fn [attribute]
+            (let [index (get (:attributes vars) attribute)]
+              (gl/glDisableVertexAttribArray index)
+              (println [:disable index (gl/glGetError) ])))
+          (keys attributes)))
+
+    ))
 
 (defn draw-kids [context kids]
   (doall (map #(apply draw-scene context %) kids)))
 
-(defmethod draw-scene :triangles [context key vertices]
-  (draw-vertices context GL_TRIANGLES vertices))
+(defmethod draw-scene :vertices [context key attr vertices]
+  (println [:vertices attr vertices])
+  (condp = (:mode attr)
+    :triangles (draw-vertices context glj/GL_TRIANGLES vertices)
+    :triangle-strip (draw-vertices context glj/GL_TRIANGLE_STRIP vertices)))
 
-(defmethod draw-scene :triangle-strip [context key vertices]
-  (draw-vertices context GL_TRIANGLE_STRIP vertices))
+(defmethod draw-scene :scene [context key attr & children]
+  (println :scene)
+  (println ["draw-scene" context children])
+  (draw-kids context children))
 
 (defmethod draw-scene :translate [context key vector & children]
+  (println :translate)
   (let [context (update-in context [:transform]
                            #(m/multiply (apply m/translate vector) %))]
     (draw-kids context children)))
 
 (defmethod draw-scene :rotate-z [context key angle & children]
+  (println :rotate-z)
   (let [context (update-in context [:transform]
                            #(m/multiply (m/rotate-z angle) %))]
     (draw-kids context children)))
 
+
 (defmethod draw-scene :scale [context key factors & children]
+  (println :scale)
   (let [context (update-in context [:transform]
                            #(m/multiply (apply m/scale factors) %))]
     (draw-kids context children)))
 
 (defmethod draw-scene :color [context key color & children]
+  (println :color)
   (let [context (update-in context [:color] (fn [x] color))]
     (draw-kids context children)))
 
 (defmethod draw-scene :group [context key attributes & children]
+  (println :group)
   (draw-kids context children))
 
-(defmethod draw-scene :texture [context key texture & children]
-  (let [name (:name texture)
-        vert (:vertices texture)
-        context (merge context
-                       {:texture-name name
-                        :texture-verts vert})]
+(defmethod draw-scene :program [context key attributes & children]
+  (println :program)
+  (let [num (:index attributes)
+        context (update-in context [:program] (fn [x] attributes))]
+    (jna/invoke Integer GLESv2/glUseProgram (int num))
     (draw-kids context children)))
 
 
+(defmethod draw-scene :texture [context key attributes & children]
+  (println [:texture attributes])
+  (let [name (:name attributes)
+        uniform (:texture (:uniforms (:program context)))]
+    ;; tell the shader which texture unit we're using
+    (println [:uniform uniform])
+    (gl/glUniform1i uniform (int 0))
+    (println (gl/glGetError))
+
+    ;; activate TEXTURE0 unit and bind our named texture into it
+    (gl/glActiveTexture glj/GL_TEXTURE0)
+    (gl/glBindTexture glj/GL_TEXTURE_2D name)
+
+    (jna/invoke Integer GLESv2/glTexParameteri
+                glj/GL_TEXTURE_2D
+                glj/GL_TEXTURE_MIN_FILTER
+                glj/GL_NEAREST)
+    (jna/invoke Integer GLESv2/glTexParameteri
+                glj/GL_TEXTURE_2D
+                glj/GL_TEXTURE_MAG_FILTER
+                glj/GL_NEAREST)
+    (println (gl/glGetError))
+
+    (draw-kids context children)))
 
 (defn paint [context scene]
   (let [context (merge
                  {:transform (m/scale 1 1 1)
-                  :color [0 1 1 1]}
+                  :color [0 0.1 0.1 1]}
                  context)]
-    (gl/glClear (int (bit-or GL_COLOR_BUFFER_BIT  GL_DEPTH_BUFFER_BIT)))
+    (gl/glClear (int (bit-or glj/GL_COLOR_BUFFER_BIT  glj/GL_DEPTH_BUFFER_BIT)))
     (apply draw-scene context scene)
     (clogl/cloglure_swap_buffers)))
 
-(defn initial-context [program]
-  {:indices
-   {:position (gl-attribute-index program "pos")
-    :color (gl-uniform-index program "color")
-    :mvp (gl-uniform-index program "modelviewProjection")}})
+
+(defn compile-glsl-in-graph [tree]
+  (let [[key attr & k] tree
+        kids #(map compile-glsl-in-graph k)]
+    (condp = key
+      :program (cons :program
+                     (cons (compile-glsl-program attr) (kids)))
+      :texture (cons :texture
+                     (cons (assoc attr :name
+                                  (glj/load-texture (:data attr)
+                                                    (:width attr)
+                                                    (:height attr)))
+                           (kids)))
+      :vertices tree
+      (cons key (cons attr (kids))))))
+
 
 (defn render-loop [chan]
-  (let [fb0 (clogl/cloglure_start "/dev/graphics/fb0")
-        program (create-shaders)
-        context (initial-context program)]
-    (println context)
-    (gl/glClear (int (bit-or GL_COLOR_BUFFER_BIT  GL_DEPTH_BUFFER_BIT)))
-    (loop []
-      (and
-       (let [scene (<!! chan)]
-         (if scene
-           (do
-             (println ["scene " scene])
-             (paint context scene)
-             true)))
-       (recur)))
+  (println [:loop chan])
+  (let [fb0 (clogl/cloglure_start "/dev/graphics/fb0")]
+    (gl/glClear (int (bit-or glj/GL_COLOR_BUFFER_BIT glj/GL_DEPTH_BUFFER_BIT)))
+    (loop [scene [:scene {} ]]
+      (when scene
+        (println ["paintig scene " scene])
+        (paint {} scene)
+        (println "done painting")
+        (recur
+         (let [[keys replacement] (<!! chan)]
+           (println ["got update at " keys])
+           (and keys
+                (update-in scene keys
+                           (fn [old] (compile-glsl-in-graph replacement))))))))
     (clogl/cloglure_stop fb0)))
 
-
-(defn start-render-thread [scene-atom]
-  (let [c (async/chan)]
-    (future (defone.ui/render-loop c))
-    (add-watch scene-atom :render
-               (fn [key ref old new] (>!! c new)))
-    c))
 (defn stop-render-thread [chan]
   (async/close! chan))
 
+(println "hi render channe")
+(defonce render-channel
+  (let [c (async/chan)]
+    (future (do
+              (println "start render thread")
+              (try (defone.ui/render-loop c)
+                   (catch Exception e (str "caught " e " in render thread")))))
+    c))
+
+(println "lo render channe")
 (defn read-raw-file [name]
   (let [f (File. name)
         l (. f length)
@@ -245,56 +220,37 @@
     (.read r buf 0 l)
     buf))
 
-(defn load-texture [data width height]
-  (let [name (int-array 1)]
-    ;; get a "name" (a.k.a number)
-    (jna/invoke Integer GLESv2/glGenTextures (int 1) name)
-    ;; make texture target TEXTURE_2D point to our new name
-    (jna/invoke Integer GLESv2/glBindTexture
-                GL_TEXTURE_2D (aget name 0))
-    ;; "Here we're setting the GL_TEXTURE_MIN_FILTER (the case where
-    ;; we have to shrink the texture for far away objects) to
-    ;; GL_NEAREST (when drawing a vertex, choose the closest
-    ;; corresponding texture pixel)."
-    (jna/invoke Integer GLESv2/glTexParameteri
-                GL_TEXTURE_2D
-                GL_TEXTURE_MIN_FILTER
-                GL_NEAREST)
-    ;; send the data across
-    (jna/invoke Integer GLESv2/glTexImage2D
-                GL_TEXTURE_2D ; target
-                (int 0)       ; detail level
-                GL_RGBA       ; internal format
-                (int width) (int height)
-                (int 0)             ; border width
-                GL_RGBA             ; data format
-                GL_UNSIGNED_BYTE    ; data type
-                data)
-    (aget name 0)))
-
-(def bath-texture
-  (load-texture (read-raw-file "/defone/bathtime.raw") 309 341))
+(def bath-texture-data (read-raw-file "/defone/bathtime.raw"))
 
 
-(def the-scene (atom
-                [:scale [0.1 0.1 0.1]
-                 [:rotate-z (* 10 (/ Math/PI 180))
-                  [:color [1 1 0 1]
-                   [:texture {:name bath-texture
-                              :vertices
-                              [[0 0] [1 0] [0 1] [1 1]]
-                              }
-                    [:group {:name :triangle}
-                     [:triangles
-                      [[-1 -1 0] [1 -1 0] [0 1 0]]
-                      ]]]]]]))
 
-(defn new-vert [scene]
-  (swap! the-scene update-in [2 2 2 2 2] (constantly scene)))
+(def my-program
+  {:attributes {:pos :vec4
+                :texture_st :vec2}
+   :uniforms {:mvp :mat4
+              :color :vec4
+              :texture :sampler2D}
+   :varyings {:v_color :vec4
+              :v_texture_st :vec2}
+   :shaders {:vertex
+             ["void main() {"
+              "   gl_Position = mvp * pos;"
+              "   v_color = color;"
+              "   v_texture_st = texture_st;"
+              "}"]
+             :fragment
+             ["void main() {"
+              "  gl_FragColor = v_color * texture2D(texture, v_texture_st);"
+              "}"]}})
 
-
+(def the-scene [:scale [0.1 0.1 0.1]
+                [:rotate-z (* 10 (/ Math/PI 180))
+                 [:color [0.8 0.8 1.0 1]
+                  [:program my-program
+                   [:texture {:data bath-texture-data :width 309 :height 341}
+                    [:vertices {:mode :triangle-strip}
+                     {:pos [[1 1 1] [5 1 1] [1 5 1] [5 5 1]]
+                      :texture_st [[0 1] [1 1] [0 0] [1 0]]}]
+                    ]]]]])
 #_
-(defone.ui/new-vert  [:triangle-strip [[0 0 0] [5 0 0] [0 5 0] [5 5 0]]])
-
-
-(defonce render-channel (start-render-thread the-scene))
+(>!! render-channel [[2] the-scene])
