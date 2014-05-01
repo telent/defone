3f62367b38c45034184608cb5467b0b111f5b752
Daniel Barlow <dan@telent.net>
2014-05-01 15:50:46 +0000

    freetype i/f now produces bitmap for a single char

--- defone/src/defone/freetype.clj
+++ defone/src/defone/freetype.clj
@@ -1,12 +1,13 @@
 (ns defone.freetype
   (:require [net.n01se.clojure-jna :as jna])
   (:import [java.io File FileInputStream]
-           [com.sun.jna Memory]))
+           [com.sun.jna Memory Pointer]))
 
 (jna/to-ns ft freetype [Integer FT_Init_FreeType
                         Integer FT_New_Face
                         Integer FT_Set_Char_Size
                         Integer FT_Get_Char_Index
+                        Integer FT_Load_Char
                         ])
 
 (defmacro checked [& args]
@@ -24,10 +25,16 @@
   (let [x (int-array 1)]
     (checked ft/FT_New_Face ft-library
              name (int 0) x)
-    (aget x 0)))
+    (Pointer. (aget x 0))))
 
 #_
-(new-face "/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans.ttf")
+(def face (new-face "/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans.ttf"))
+
+#_
+(seq (let [r (int-array 10)] (.read (.getPointer (.getPointer face 84) 76)w 0 r 0 5) r))
+
+
+
 
 (defn set-char-size [face width height hdpi vdpi]
   (checked ft/FT_Set_Char_Size
@@ -38,4 +45,42 @@
            vdpi))
 
 (defn get-char-index [face code]
-  (ft/FT_Get_Char_Index face code))
+  (ft/FT_Get_Char_Index (int face) (int code)))
+
+
+(def FT_LOAD_RENDER (bit-shift-left 1 2))
+
+(defn load-char [face code]
+  (checked ft/FT_Load_Char face (int code) FT_LOAD_RENDER))
+
+(defn bitmap-from-pointer [buf]
+  {:rows (.getInt buf 0)
+   :width (.getInt buf 4)
+   :pitch (.getInt buf 8)
+   :data (.getPointer buf 12)
+   :num-grays (.getShort buf 16)
+   :pixel-mode (.getByte buf 18)
+   :palette-mode (.getByte buf 19)
+   :palette (.getPointer buf 20)
+   })
+
+(def face-glyph-offset 84)
+(def glyph-bitmap-offset 76)
+
+(defn render-char [face size char]
+  (set-char-size face size 0 316 316)
+  (load-char face char)
+  (let [glyph-slot (.getPointer face face-glyph-offset)]
+    (bitmap-from-pointer (.share glyph-slot glyph-bitmap-offset))))
+
+#_
+(defn frog [string bitmap x y]
+  (let [face (new-face "/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans.ttf")]
+    (set-char-size face 12 0 316 316)))
+;#    (doall (map #(get-char-index face %) string)
+;#      load-glyph
+;#      render-glyph
+;#      slot=face->glyph
+;#      slot->bitmap_top is distance from baseline to
+;#      face->glyph->bitmap, face->glyph->bitmap_left, face->glyph->bitmap_top
+;#     glyph-indexes)))
