ee9c650a47d2dc68ded7395342f9ceb076d9f99c
Daniel Barlow <dan@telent.net>
2014-05-12 23:59:15 +0100

    upgraded to cider on noetbook, need this apparently

--- defone/project.clj
+++ defone/project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repl-options {; :init-ns defone.core
                  :timeout 120000}
+  :plugins [[cider/cider-nrepl "0.6.0"]]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.nrepl "0.2.3"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
