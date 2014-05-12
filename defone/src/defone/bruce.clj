f0f7564d76db8c31a6a105d8dff67e0311b2d513
Daniel Barlow <dan@telent.net>
2014-05-12 23:47:50 +0100

    POC to get structure layout info from headers via cc
    Use define-c-record to specify each struct and element name that
    you want size and offset information for, and it will create, build
    and run a small C program using sizeof and offsetof that spits
    out that information, then read the results into `grovelled-types'
    which you can use as grist for your FFI of choice

---
+++ defone/src/defone/bruce.clj
@@ -0,0 +1,99 @@
+(ns defone.bruce
+  (:refer-clojure)
+  (:require [clojure.edn :as edn]
+            [clojure.java.shell :as shell]))
+
+;;;; Welcome to Bruce Groveller
+
+;;; extracts structure layouts from C header files by compiling small
+;;; C programs that refer to them and that produce output in some
+;;; format that Clojure can understand
+
+(def types-to-grovel (atom {}))
+
+(defn- canonize-member [options m]
+  (let [name (if (coll? m) (first m) m)
+        aliases (if (coll? m) (conj (rest m) name) [name])]
+    (list
+     (clojure.string/join [(get options :prefix "") name])
+     (vec aliases))))
+
+(defmacro define-c-record [record-name header-files options & members]
+  (let [n {
+           :record-name (name record-name)
+           :header-files header-files
+           :members `(quote ~(map (partial canonize-member options) members))
+           :options options
+           }]
+    `(swap! types-to-grovel assoc ~(name record-name) ~n)
+    ))
+
+(define-c-record stat ["sys/types.h" "sys/stat.h" "unistd.h"]
+  {;; :search-path ["/usr/include"]
+   ;; :compiler-flags (:out (shell/sh "pkg-config ftgl --cflags --libs"))
+   :compiler "gcc"
+   :prefix "st_"
+   }
+  [dev device] ; for each member, name as known to C then aliases
+  [ino inode]
+  mode
+  nlink
+  uid
+  gid
+  size
+  atime
+  mtime
+  ctime)
+
+(defn headers-for [specs]
+  (distinct (mapcat :header-files (vals specs))))
+
+(defn grovel-struct [struct-spec]
+  (let [s (:record-name struct-spec)]
+    (clojure.string/join
+     "\n"
+     (map #(str "    printf(\"[" s " %s %d %d]\\n\", "
+                "\"" (first %) "\""
+                ", "
+                "offsetof(struct " s ", " (first %) ")"
+                ", "
+                "member_sizeof(struct " s ", " (first %) ")"
+                ");")
+          (:members struct-spec)))))
+
+
+(defn groveller-program [record-specs]
+  (concat (map #(str "#include <" % ">")
+               (conj (headers-for record-specs) "stddef.h"))
+          [
+           "#define member_sizeof(type, member) sizeof(((type *)0)->member)"
+           "int main() {"
+           "    printf(\"[\");"
+           ]
+          (map grovel-struct (vals record-specs))
+          [
+           "    printf(\"]\");"
+           "    exit(0);"
+           "}"]))
+
+(defn grovel [record-specs]
+  (let [tmpnam (java.io.File/createTempFile "grovel_" ".c")
+        tmpout (java.io.File/createTempFile "grovel_" ".out")
+        cc-line (concat ["/usr/bin/gcc"
+                         "-o" (.getPath tmpout)
+                         (.getPath tmpnam)]
+                        (distinct (filter identity
+                                          (map #(get % :compiler-flags)
+                                               (vals record-specs)))))]
+    (spit tmpnam (clojure.string/join "\n" (groveller-program record-specs)))
+    (let [cc (apply shell/sh cc-line)]
+      (if (zero? (:exit cc))
+        (let [result (shell/sh (.getPath tmpout))]
+          (and (zero? (:exit result))
+               (reduce (fn [m [record el size offset]]
+                         (assoc-in m [record el] [size offset]))
+                       {}
+                       (edn/read-string (:out result)))))
+        cc))))
+
+(def grovelled-types (grovel @types-to-grovel))
